d7c107adc0ea46aed9ce872448005f051f261473
bugszhou <bugszhou@outlook.com>
2019-04-20 23:27:48 +0800

    fix: split Css

--- src/index.js
+++ src/index.js
@@ -27,11 +27,14 @@ function getEntry({
     throw new Error('Entry must be json string!');
   }
   appJson.pages.unshift('app');
-  return parseEntry({baseUrl: './src', entryFile: appJson, cssSuffix, xmlSuffix});
+  return parseEntry({ baseUrl: './src', entryFile: appJson, cssSuffix, xmlSuffix });
 }
 
 class MiniappAutoPlugin {
-  constructor(options = {}) {
+  constructor(options = {
+    cssSuffix: 'css',
+    xmlSuffix: 'html',
+  }) {
     this.options = options
   }
 
@@ -41,11 +44,31 @@ class MiniappAutoPlugin {
     compiler.hooks.emit.tap(
       'miniapp-webpack-plugin-emit',
       (compilation) => {
-        const {entrypoints, assets} = compilation || {};
+        const { entrypoints, assets } = compilation || {};
         let commons = this.getCommonSplit(entrypoints);
         Object.keys(assets).forEach((pathurl) => {
-          if (/\.js$/.test(pathurl) && commons.indexOf(pathurl) < 0) {
-            let commonStrArr = this.getRelativePath(pathurl, commons);
+          const unixPathurl = pathurl.split('\\').join('\/'),
+            entryCommon = commons[unixPathurl.replace(new RegExp(`\.(${this.options.cssSuffix}|js)$`), '')];
+          if (!entryCommon) {
+            return false;
+          }
+          const { js, css} = entryCommon,
+            jsLen = js.length,
+            cssLen = css.length;
+
+          if (/\.js$/.test(pathurl) && js.indexOf(unixPathurl) < 0 && jsLen) {
+            let commonStrArr = this.getRelativePath(pathurl, js);
+            if (assets[pathurl]._value) {
+              assets[pathurl]._value = commonStrArr.join('') + assets[pathurl]._value;
+            } else {
+              if (assets[pathurl].children && Array.isArray(assets[pathurl].children)) {
+                assets[pathurl].children[0]._value = commonStrArr.join('') + assets[pathurl].children[0]._value;
+              } else {
+                assets[pathurl]._source.children.unshift(commonStrArr.join(''));
+              }
+            }
+          } else if (cssLen && (new RegExp(`\.${this.options.cssSuffix}$`)).test(pathurl) && css.indexOf(unixPathurl) < 0) {
+            let commonStrArr = this.getRelativeCssPath(pathurl, css);
             if (assets[pathurl]._value) {
               assets[pathurl]._value = commonStrArr.join('') + assets[pathurl]._value;
             } else {
@@ -61,18 +84,32 @@ class MiniappAutoPlugin {
   }
 
   getCommonSplit(entrypoints = []) {
-    let commons = [];
+    let commons = {};
     entrypoints.forEach((entrypoint, file) => {
-      const chunks = entrypoint.chunks || [];
+      const chunks = entrypoint.chunks || [],
+        name = entrypoint.options.name,
+        depModules = {
+          js: [],
+          css: [],
+        };
       chunks.forEach(c => {
         if (c.chunkReason) {
-          let splitFile = c.files[0],
-            isJs = /\.js$/.test(splitFile);
-          if (isJs && commons.indexOf(splitFile) < 0) {
-            commons.push(splitFile.split(path.sep).join('\/'));
+          let [splitFile, secFile] = c.files,
+            isJs = /\.js$/.test(splitFile),
+            isCss = (new RegExp(`\.${this.options.cssSuffix}$`)).test(splitFile);
+
+          if (isJs && depModules.js.indexOf(splitFile) < 0) {
+            depModules.js.push(splitFile.split(path.sep).join('\/'));
+          }
+          if (isCss && depModules.css.indexOf(splitFile) < 0) {
+            if (/\.js$/.test(secFile) && depModules.js.indexOf(secFile) < 0) {
+              depModules.js.push(secFile.split(path.sep).join('\/'));
+            }
+            depModules.css.push(splitFile.split(path.sep).join('\/'));
           }
         }
       });
+      commons[name] = depModules;
     });
     return commons;
   }
@@ -80,7 +117,14 @@ class MiniappAutoPlugin {
   getRelativePath(url, commons = []) {
     const resolveUrl = path.resolve(process.cwd(), path.dirname(url));
     return commons.map((common) => {
-      return `require("./${path.relative(resolveUrl, path.resolve(process.cwd(), common)).split('\\').join('\/')}");\n\n\n`;
+      return `require("./${path.relative(resolveUrl, path.resolve(process.cwd(), common)).split('\\').join('\/')}");\n`;
+    });
+  }
+
+  getRelativeCssPath(url, commons = []) {
+    const resolveUrl = path.resolve(process.cwd(), path.dirname(url));
+    return commons.map((common) => {
+      return `@import "./${path.relative(resolveUrl, path.resolve(process.cwd(), common)).split('\\').join('\/')}";\n\n`;
     });
   }
 }
