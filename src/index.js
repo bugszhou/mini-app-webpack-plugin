aed644e9518ce11714f12f56e3e85bc5ca1d1a57
bugszhou <bugszhou@outlook.com>
2019-04-26 18:31:26 +0800

    feat(all): Add mini css

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const pluginName = 'mini-app-webpack-plugin',
 function getEntry({
                     entry = './src/app.json',
                     cssSuffix = 'css',
+                    compileCssSuffix = 'css',
                     xmlSuffix = 'html'
                   } = {}) {
   const entryFile = path.resolve(process.cwd(), entry);
@@ -27,14 +28,11 @@ function getEntry({
     throw new Error('Entry must be json string!');
   }
   appJson.pages.unshift('app');
-  return parseEntry({ baseUrl: './src', entryFile: appJson, cssSuffix, xmlSuffix });
+  return parseEntry({baseUrl: './src', entryFile: appJson, cssSuffix, compileCssSuffix, xmlSuffix});
 }
 
 class MiniappAutoPlugin {
-  constructor(options = {
-    cssSuffix: 'css',
-    xmlSuffix: 'html',
-  }) {
+  constructor(options = {}) {
     this.options = options
   }
 
@@ -44,31 +42,11 @@ class MiniappAutoPlugin {
     compiler.hooks.emit.tap(
       'miniapp-webpack-plugin-emit',
       (compilation) => {
-        const { entrypoints, assets } = compilation || {};
+        const {entrypoints, assets} = compilation || {};
         let commons = this.getCommonSplit(entrypoints);
         Object.keys(assets).forEach((pathurl) => {
-          const unixPathurl = pathurl.split('\\').join('\/'),
-            entryCommon = commons[unixPathurl.replace(new RegExp(`\.(${this.options.cssSuffix}|js)$`), '')];
-          if (!entryCommon) {
-            return false;
-          }
-          const { js, css} = entryCommon,
-            jsLen = js.length,
-            cssLen = css.length;
-
-          if (/\.js$/.test(pathurl) && js.indexOf(unixPathurl) < 0 && jsLen) {
-            let commonStrArr = this.getRelativePath(pathurl, js);
-            if (assets[pathurl]._value) {
-              assets[pathurl]._value = commonStrArr.join('') + assets[pathurl]._value;
-            } else {
-              if (assets[pathurl].children && Array.isArray(assets[pathurl].children)) {
-                assets[pathurl].children[0]._value = commonStrArr.join('') + assets[pathurl].children[0]._value;
-              } else {
-                assets[pathurl]._source.children.unshift(commonStrArr.join(''));
-              }
-            }
-          } else if (cssLen && (new RegExp(`\.${this.options.cssSuffix}$`)).test(pathurl) && css.indexOf(unixPathurl) < 0) {
-            let commonStrArr = this.getRelativeCssPath(pathurl, css);
+          if (/\.js$/.test(pathurl) && commons.indexOf(pathurl) < 0) {
+            let commonStrArr = this.getRelativePath(pathurl, commons);
             if (assets[pathurl]._value) {
               assets[pathurl]._value = commonStrArr.join('') + assets[pathurl]._value;
             } else {
@@ -84,32 +62,18 @@ class MiniappAutoPlugin {
   }
 
   getCommonSplit(entrypoints = []) {
-    let commons = {};
+    let commons = [];
     entrypoints.forEach((entrypoint, file) => {
-      const chunks = entrypoint.chunks || [],
-        name = entrypoint.options.name,
-        depModules = {
-          js: [],
-          css: [],
-        };
+      const chunks = entrypoint.chunks || [];
       chunks.forEach(c => {
         if (c.chunkReason) {
-          let [splitFile, secFile] = c.files,
-            isJs = /\.js$/.test(splitFile),
-            isCss = (new RegExp(`\.${this.options.cssSuffix}$`)).test(splitFile);
-
-          if (isJs && depModules.js.indexOf(splitFile) < 0) {
-            depModules.js.push(splitFile.split(path.sep).join('\/'));
-          }
-          if (isCss && depModules.css.indexOf(splitFile) < 0) {
-            if (/\.js$/.test(secFile) && depModules.js.indexOf(secFile) < 0) {
-              depModules.js.push(secFile.split(path.sep).join('\/'));
-            }
-            depModules.css.push(splitFile.split(path.sep).join('\/'));
+          let splitFile = c.files[0],
+            isJs = /\.js$/.test(splitFile);
+          if (isJs && commons.indexOf(splitFile) < 0) {
+            commons.push(splitFile.split(path.sep).join('\/'));
           }
         }
       });
-      commons[name] = depModules;
     });
     return commons;
   }
@@ -117,14 +81,7 @@ class MiniappAutoPlugin {
   getRelativePath(url, commons = []) {
     const resolveUrl = path.resolve(process.cwd(), path.dirname(url));
     return commons.map((common) => {
-      return `require("./${path.relative(resolveUrl, path.resolve(process.cwd(), common)).split('\\').join('\/')}");\n`;
-    });
-  }
-
-  getRelativeCssPath(url, commons = []) {
-    const resolveUrl = path.resolve(process.cwd(), path.dirname(url));
-    return commons.map((common) => {
-      return `@import "./${path.relative(resolveUrl, path.resolve(process.cwd(), common)).split('\\').join('\/')}";\n\n`;
+      return `require("./${path.relative(resolveUrl, path.resolve(process.cwd(), common)).split('\\').join('\/')}");\n\n\n`;
     });
   }
 }
--- src/lib/parseEntry.js
+++ src/lib/parseEntry.js
@@ -13,7 +13,7 @@ const path = require('path'),
  * wxs ==> [wxs(js)] 单独一个webpack
  */
 
-module.exports = function({baseUrl = './src', entryFile = '', cssSuffix, xmlSuffix} = {}) {
+module.exports = function({baseUrl = './src', entryFile = '', cssSuffix, xmlSuffix, compileCssSuffix} = {}) {
 
   let pages = entryFile.pages,
     subpackages = entryFile.subpackages;
@@ -28,7 +28,7 @@ module.exports = function({baseUrl = './src', entryFile = '', cssSuffix, xmlSuff
     });
     pages = pages.concat(tmp);
   }
-  return getBaseEntry(pages, cssSuffix, xmlSuffix, baseUrl);
+  return getBaseEntry(pages, cssSuffix, xmlSuffix, baseUrl, compileCssSuffix);
 };
 
 function hasSubPackages(packages) {
@@ -39,7 +39,7 @@ function isPlugin(url) {
   return /^plugin\:\/\//.test(url);
 }
 
-function getBaseEntry(pages = [], cssSuffix, xmlSuffix, baseUrl) {
+function getBaseEntry(pages = [], cssSuffix, xmlSuffix, baseUrl, compileCssSuffix) {
   const cwd = process.cwd(),
     entry = {},
     entryJsonFiles = {};
@@ -47,17 +47,18 @@ function getBaseEntry(pages = [], cssSuffix, xmlSuffix, baseUrl) {
     if (isPlugin(page)) {
       return false;
     }
-    let {jsPath, xml, css, json} = getEntryFileUrl(page, cwd, baseUrl, cssSuffix, xmlSuffix);
-    let {pageEntry, jsonFiles} = getEntry([jsPath, xml, css, json], baseUrl, cssSuffix, xmlSuffix);
+    let {jsPath, xml, css, json, ocss} = getEntryFileUrl(page, cwd, baseUrl, cssSuffix, xmlSuffix, compileCssSuffix);
+    let {pageEntry, jsonFiles} = getEntry([jsPath, xml, css, json, ocss], baseUrl, cssSuffix, xmlSuffix, compileCssSuffix);
     if (exists(json.reourcePath)) {
       let {entryName, data} = getJsonComponents(json.reourcePath, JSON.parse(fs.readFileSync(json.reourcePath).toString() || '{}'), {
         cssSuffix,
+        compileCssSuffix,
         xmlSuffix,
         cwd,
         baseUrl
       });
       if (entryName) {
-        let componentsEntryObj = getEntry([data.jsPath, data.xml, data.css, data.json], baseUrl, cssSuffix, xmlSuffix);
+        let componentsEntryObj = getEntry([data.jsPath, data.xml, data.css, data.json, data.ocss], baseUrl, cssSuffix, xmlSuffix, compileCssSuffix);
         let componentsEntry = componentsEntryObj.pageEntry;
         let componentsJsonFiles = componentsEntryObj.jsonFiles;
         if (componentsEntry.length) {
@@ -81,7 +82,7 @@ function getBaseEntry(pages = [], cssSuffix, xmlSuffix, baseUrl) {
   };
 }
 
-function getEntryFileUrl(page, cwd, baseUrl, cssSuffix, xmlSuffix) {
+function getEntryFileUrl(page, cwd, baseUrl, cssSuffix, xmlSuffix, compileCssSuffix) {
   return {
     jsPath: {
       reourcePath: path.resolve(cwd, `${baseUrl}/${page}.js`),
@@ -92,6 +93,10 @@ function getEntryFileUrl(page, cwd, baseUrl, cssSuffix, xmlSuffix) {
       entry: `${page}.${xmlSuffix}`
     },
     css: {
+      reourcePath: path.resolve(cwd, `${baseUrl}/${page}.${compileCssSuffix}`),
+      entry: `${page}.${compileCssSuffix}`
+    },
+    ocss: {
       reourcePath: path.resolve(cwd, `${baseUrl}/${page}.${cssSuffix}`),
       entry: `${page}.${cssSuffix}`
     },
@@ -102,12 +107,12 @@ function getEntryFileUrl(page, cwd, baseUrl, cssSuffix, xmlSuffix) {
   };
 }
 
-function getEntry(entry = [], baseUrl = './src', cssSuffix, xmlSuffix) {
+function getEntry(entry = [], baseUrl = './src', cssSuffix, xmlSuffix, compileCssSuffix) {
   let pageEntry = [],
     jsonFiles = [];
   entry.forEach(({reourcePath, entry}) => {
     let entryUrl = `${baseUrl}/${entry}`;
-    if (new RegExp(`\.(${cssSuffix}|json)$`).test(entry)) {
+    if (new RegExp(`\.(${cssSuffix}|${compileCssSuffix}|json)$`).test(entry)) {
       if (exists(reourcePath)) {
         pageEntry.push(entryUrl);
         if (/\.json$/.test(entry)) {
@@ -141,7 +146,7 @@ function getJsonComponents(resourcePath, json = {}, options = {}) {
     let entryKey = getRequire(resourcePath, url);
     return {
       entryName: entryKey,
-      data: getEntryFileUrl(entryKey, options.cwd, options.baseUrl, options.cssSuffix, options.xmlSuffix)
+      data: getEntryFileUrl(entryKey, options.cwd, options.baseUrl, options.cssSuffix, options.xmlSuffix, options.compileCssSuffix)
     };
   })[0] || {};
 }
