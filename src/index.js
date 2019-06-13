ad28f67553a11da382984c990a8c80c083f79a2d
bugszhou <bugszhou@outlook.com>
2019-06-13 18:37:17 +0800

    fix(index): _source is null

--- src/index.js
+++ src/index.js
@@ -77,7 +77,11 @@ class MiniappAutoPlugin {
               if (assets[pathurl].children && Array.isArray(assets[pathurl].children)) {
                 assets[pathurl].children[0]._value = commonStrArr.join('') + assets[pathurl].children[0]._value;
               } else {
-                assets[pathurl]._source.children.unshift(commonStrArr.join(''));
+                if (assets[pathurl]._source) {
+                  assets[pathurl]._source.children.unshift(commonStrArr.join(''));
+                } else {
+                  assets[pathurl]._value = commonStrArr.join('') + assets[pathurl]._value;
+                }
               }
             }
           }
