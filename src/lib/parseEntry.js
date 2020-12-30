f0cd4a4d5d3e712fb721f652c7e9be77b331c5e3
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-12-30 11:16:08 +0800

    feat: 支持组件引用组件

--- src/lib/parseEntry.js
+++ src/lib/parseEntry.js
@@ -83,6 +83,7 @@ function getBaseEntry(
       xmlSuffix,
       compileCssSuffix,
     );
+
     if (exists(json.reourcePath)) {
       let components = getJsonComponents(
         json.reourcePath,
@@ -187,35 +188,65 @@ function getEntry(
   };
 }
 
+/**
+ * 解析component path
+ * @param {string} resourcePath 引用组件的文件绝对路径
+ * @param {Object} json 页面或者组件的json配置
+ * @param {Object} options 文件后缀信息
+ */
 function getJsonComponents(resourcePath, json = {}, options = {}) {
   let isUseComponent =
     isObject(json.usingComponents) && !isEmpty(json.usingComponents);
   if (!isUseComponent) {
     return [];
   }
-  return (
-    Object.entries(json.usingComponents).map(([componentName, url]) => {
-      if (isPlugin(url) || url.includes("weui-miniprogram")) {
-        return {
-          entryName: "",
-          data: {},
-        };
-      }
-      let entryKey = getRequire(resourcePath, url);
+  const results = {};
+  findComponents(resourcePath, json, options, results);
+  return Object.values(results || {});
+}
+
+/**
+ * 遍历解析
+ * @param {string} resourcePath 引用组件的文件绝对路径
+ * @param {Object} json 页面或者组件的json配置
+ * @param {Object} options 文件后缀信息
+ */
+function findComponents(resourcePath, json = {}, options = {}, returnData = {}) {
+  let isUseComponent =
+    isObject(json.usingComponents) && !isEmpty(json.usingComponents);
+  if (!isUseComponent) {
+    return;
+  }
+  Object.entries(json.usingComponents).forEach(([componentName, url]) => {
+    if (isPlugin(url) || url.includes("weui-miniprogram")) {
       return {
-        entryName: entryKey,
-        data: getEntryFileUrl(
-          entryKey,
-          options.cwd,
-          options.baseUrl,
-          options.cssSuffix,
-          options.xmlSuffix,
-          options.compileCssSuffix,
-          options.jsSuffix,
-        ),
+        entryName: "",
+        data: {},
       };
-    }) || []
-  );
+    }
+    let entryKey = getRequire(resourcePath, url);
+    console.log("getNodeModulesSource ===> ", getNodeModulesSource(resourcePath, url));
+    const data = {
+      entryName: entryKey,
+      data: getEntryFileUrl(
+        getNodeModulesSource(resourcePath, url, entryKey),
+        options.cwd,
+        options.baseUrl,
+        options.cssSuffix,
+        options.xmlSuffix,
+        options.compileCssSuffix,
+        options.jsSuffix,
+      ),
+    };
+    returnData[data.entryName] = data;
+    const jsonPath = data.data.json.reourcePath;
+    if (exists(jsonPath)) {
+      const jsonData = JSON.parse(fs.readFileSync(jsonPath).toString() || "{}");
+      if (jsonData.usingComponents) {
+        findComponents(jsonPath, jsonData, options, returnData);
+      }
+    }
+  });
 }
 
 function eachJsonComponents(
@@ -263,4 +294,20 @@ function getRequire(resourcePath, url) {
   }
 
   return url.replace("/", "");
+}
+
+function getNodeModulesSource(resourcePath, url, entryKey) {
+  const isRootUrl = url.indexOf("/") === 0,
+    fileDir = path.dirname(resourcePath),
+    srcName = path.relative(process.cwd(), fileDir).split(path.sep)[0] || "src";
+
+  if (srcName !== "node_modules") {
+    return entryKey;
+  }
+
+  if (!isRootUrl) {
+    return path.relative(process.cwd(), path.resolve(fileDir, url));
+  }
+
+  return entryKey;
 }
